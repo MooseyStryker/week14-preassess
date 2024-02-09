c675971561e1585183b509e2c4533055e5001dd9
Stryker Huber <stryker.important@gmail.com>
2024-02-09 10:45:59 -0700

    Finished FruitsIndex

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,90 @@
 import fruits from './mockData/fruits.json';
+import Navigation from './components/Navigation';
+import FruitsIndex from './components/FruitsIndex';
+import FruitShow from './components/FruitShow';
+import FruitForm from './components/FruitForm';
+import FavoriteFruit from './components/FavoriteFruit';
+import SetFavoriteFruit from './components/SetFavoriteFruit';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 function App() {
-  return <h1>Welcome to Fruits App</h1>;
+
+
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path = "/" element={< FruitsIndex />} fruits={ fruits }>
+  //       <Route path = "/fruits/:fruitId" element={< FruitShow />} fruits={ fruits }/>
+  //       <Route path = "/fruits/new" element={< FruitForm />} fruits={ fruits }/>
+  //       <Route path = "/fav-fruit" element={< FavoriteFruit />} fruits={ fruits }/>
+  //       <Route path = "/set-fav-fruit" element={< SetFavoriteFruit />} fruits={ fruits }/>
+  //       <Route path = "*" element={<h2>"Page Not Found</h2>} />
+  //     </Route>
+  //   )
+  // )
+
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <Navigation />
+          <FruitsIndex fruits={fruits}/>
+        </>
+      ),
+    },
+    {
+      path: '/fruits/:fruitId',
+      element: (
+        <>
+          <Navigation />
+          <FruitShow fruits={fruits}/>
+        </>
+      ),
+    },
+    {
+      path: '/fruits/new',
+      element: (
+        <>
+          <Navigation />
+          <FruitForm fruits={fruits}/>
+        </>
+      ),
+    },
+    {
+      path: '/fav-fruit',
+      element: (
+        <>
+          <Navigation />
+          <FavoriteFruit fruits={fruits}/>
+        </>
+      ),
+    },
+    {
+      path: '/set-fav-fruit',
+      element: (
+        <>
+          <Navigation />
+          <SetFavoriteFruit fruits={fruits}/>
+        </>
+      ),
+    },
+    {
+      path: '*',
+      element: (
+        <>
+          <Navigation/>,
+          <h2>Page not found </h2>
+        </>
+      ),
+    },
+  ])
+
+
+
+  return (
+    <RouterProvider router={router} />
+    );
 }
 
 export default App;
